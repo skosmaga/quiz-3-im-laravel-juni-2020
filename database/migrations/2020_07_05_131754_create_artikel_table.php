d1203dec7b962224784f91e4006129f63b877e77
skosmaga <shinta.kosmaga@gmail.com>
2020-07-05 20:44:20 +0700

    up to layout

---
+++ database/migrations/2020_07_05_131754_create_artikel_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArtikelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artikel', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('judul');
+            $table->longText('isi');
+            $table->string('slug');
+            $table->string('tag');
+            $table->integer('userid');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artikel');
+    }
+}
